675a2500a6d05cc036e3df16cfaa324eba7ea317
ridanaeeem <ridan@bu.edu>
2023-10-14 15:43:39 -0400

    fix css and add js

---
+++ projects/mini-projects/mp3/ridan-interactive-resume.js
@@ -0,0 +1,9 @@
+const userPass = {
+    "hello123": "ridan",
+    "tboy123": "jeffrey",
+    "100sleep": "pierce",
+    "h1gh":"arctic",
+    "d0ldrum":"boy",
+    "d1vide":"chemical",
+    "myl0ve":"mitski"
+};
